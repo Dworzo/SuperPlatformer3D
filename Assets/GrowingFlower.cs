ec5708d33fa18ac54bc46d68ecde2fa6722d27a5
Dworzo <dworzo@gmail.com>
2022-05-28 19:52:54 +0200

    Random levels

--- Assets/GrowingFlower.cs
+++ Assets/GrowingFlower.cs
@@ -7,7 +7,7 @@ public class GrowingFlower : MonoBehaviour
 {
     private float coin = 0;
     public Collider flowerCollider;
-    public TextMeshProUGUI textCoins;
+    
 
     
     private void OnTriggerEnter(Collider other)
@@ -15,8 +15,8 @@ public class GrowingFlower : MonoBehaviour
         if (other.transform.tag == "Player")
         {
             Debug.Log("ZebralemMonete");
-            coin++;
-            textCoins.text = coin.ToString();
+            
+          
             //Destroy(other.gameObject);
             transform.localScale = new Vector3(4, 4, 4);
             flowerCollider.enabled = !flowerCollider.enabled;
