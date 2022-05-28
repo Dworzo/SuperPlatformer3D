541cc00d875db37a5418252cfd813f898e94bce2
Dworzo <dworzo@gmail.com>
2022-05-28 16:43:44 +0200

    Game Jam

--- Assets/GrowingFlower.cs
+++ Assets/GrowingFlower.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class CoinPicker : MonoBehaviour
+public class GrowingFlower : MonoBehaviour
 {
     private float coin = 0;
-
+    public Collider flowerCollider;
     public TextMeshProUGUI textCoins;
 
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Coin")
+        if (other.transform.tag == "Player")
         {
             Debug.Log("ZebralemMonete");
             coin++;
             textCoins.text = coin.ToString();
             //Destroy(other.gameObject);
-           
+            transform.localScale = new Vector3(4, 4, 4);
+            flowerCollider.enabled = !flowerCollider.enabled;
+
         }
             
     }
